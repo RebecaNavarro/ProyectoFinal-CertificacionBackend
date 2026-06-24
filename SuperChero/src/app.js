c0c693a9d6b8b044be296302d0721dfbfa7b21d8
RebecaNavarro <becanavarro2003@gmail.com>
2026-06-24 09:08:23 -0400

    Separar server y app

--- SuperChero/src/app.js
+++ SuperChero/src/app.js
@@ -1,22 +1,14 @@
-import dotenv from "dotenv";
 import express from "express";
-import { connectDB } from "./data/mongoConnection.js";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./docs/swagger.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import clothesRoutes from "./routes/clothesRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from "./docs/swagger.js";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-await connectDB();
 
 app.use(express.json());
 
@@ -32,6 +24,6 @@ app.use("/api/users", userRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use(errorHandler);
+
+export default app;
